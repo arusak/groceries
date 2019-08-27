ba10b7dced154365d4b0184fd9b019e7263e0eb6
arusak <anton.rusak@gmail.com>
2019-08-27 13:25:14 +0300

    Firestore included

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAFaAnv1pe1mYeP9DeLCnrVx8oTwEURNng',
+    authDomain: 'groceries-v1.firebaseapp.com',
+    databaseURL: 'https://groceries-v1.firebaseio.com',
+    projectId: 'groceries-v1',
+    storageBucket: '',
+    messagingSenderId: '233900766247',
+    appId: '1:233900766247:web:19bb288db6bbbe70'
+  }
 };
 
 /*
