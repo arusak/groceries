3c826fb60741165099c1f129621ffa7b70ce9180
arusak <anton.rusak@gmail.com>
2019-09-12 12:11:27 +0300

    Enhancement. Show count of usages in history

--- src/app/models/history-item.model.ts
+++ src/app/models/history-item.model.ts
@@ -1,20 +1,19 @@
 export class HistoryItemModel {
-  readonly id: string;
-  readonly title: string;
-
-  private count: number;
+  static increaseCount(item) {
+    return new HistoryItemModel(item.title, item.count + 1, item.id);
+  }
 
   static createByTitle(title: string) {
     return new HistoryItemModel(title, 1);
   }
 
+  readonly id: string;
+  readonly title: string;
+  readonly count: number;
+
   constructor(title: string, count: number, id?: string) {
     this.id = id;
     this.title = title;
     this.count = count;
-  }
-
-  static increaseCount(item) {
-    return new HistoryItemModel(item.title, item.count + 1, item.id);
   }
 }
